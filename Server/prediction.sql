b1284baeaf3054ec5e5c02bb69e38ffab2463a6e
Faysal598 <faysalmahmud735@gmail.com>
2022-08-27 14:21:53 -0700

    comment added in prediction.sql

--- Server/prediction.sql
+++ Server/prediction.sql
@@ -1,9 +1,11 @@
 SET SERVEROUTPUT ON;
 SET VERIFY OFF;
 
+--Delete previous difference_table from all sites
 Delete from difference_table1@site1;
 Delete from difference_table2@site2;
 
+--Function for determine distance
 CREATE OR REPLACE FUNCTION absolute_value(A IN NUMBER, B IN NUMBER)
 RETURN NUMBER
 IS 
@@ -15,7 +17,7 @@ BEGIN
 END absolute_value;
 /
 
-
+--Input from console
 ACCEPT P NUMBER PROMPT "Enter your preferable memory in GigaByte: "
 ACCEPT Q NUMBER PROMPT "Enter your preferable storage in GigaByte: "
 ACCEPT R NUMBER PROMPT "Enter your preferable cpu_speed in Hz: "
@@ -30,13 +32,15 @@ DECLARE
 	D number:=&S;
 	E FLOAT:=&T;
 	I number:=&X;
-		
+	
+	--For raise exception
 	invalid_memory EXCEPTION;
 	invalid_storage EXCEPTION;
 	invalid_CPUSpeed EXCEPTION;
 	invalid_Battery EXCEPTION;
 	invalid_Display EXCEPTION;
 
+--Condition for exception
 BEGIN
 	IF A>32 OR A<4 THEN
 		RAISE invalid_memory;
@@ -50,6 +54,7 @@ BEGIN
 		RAISE invalid_Display;
 	END IF;
 	
+	--Takes data from site1
 	IF A = 4 OR A = 8 THEN
 		predict_laptop_site1.nearestNeighbours(A,B,C,D,E);
 		
@@ -69,9 +74,9 @@ BEGIN
 			ELSE
 				DBMS_OUTPUT.PUT_LINE('YOU DONT HAVE A CHOICE');	
 		END CASE;
-		
-	ELSIF A = 16 OR A = 32 THEN
 	
+	--Takes data from site2
+	ELSIF A = 16 OR A = 32 THEN
 		predict_laptop_site2.nearestNeighbours(A,B,C,D,E);
 		
 		CASE (I)
@@ -91,7 +96,8 @@ BEGIN
 				DBMS_OUTPUT.PUT_LINE('YOU DONT HAVE A CHOICE');		
 		END CASE;
 	END IF;
-			
+	
+	--Exception message print
 	EXCEPTION
 		WHEN invalid_memory THEN
 			DBMS_OUTPUT.PUT_LINE('Invalid memory');
